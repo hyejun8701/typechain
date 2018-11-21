3a9308a7a390b695fe00fe83a399d5b60c55591e
hyejun8701 <hyejun8701@gmail.com>
2018-11-21 15:56:00 +0900

    class

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,33 @@
-const sayHi = (name:string, age: number, gender:string): string => {
-    return `Hello ${name}, you are ${age}, yuo are a ${gender}!`;
+// interface Human {
+//     name: string,
+//     age: number,
+//     gender: string
+// }
+
+class Human {
+    public name: string;
+    public age: number;
+    public gender: string;
+
+    constructor(name: string, age:number, gender: string) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+}
+
+// const person = {
+//     name: "yoon",
+//     age: 32,
+//     gender: "f"
+// }
+
+const yoon = new Human("yoon", 32, "f")
+
+const sayHi = (person: Human): string => {
+    return `Hello ${person.name}, you are ${person.age}, yuo are a ${person.gender}!!`;
 };
 
-console.log(sayHi("Yoon", 32, "female"));
+console.log(sayHi(yoon));
 
 export {};
